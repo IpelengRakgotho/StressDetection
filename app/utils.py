14b726f7852da4bd4fad2b5c1e91acfaa808ae75
Lerato <lerato.rakgotho@gmail.com>
2024-11-11 21:39:56 +0200

    editing the path

--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
-import os
 
 def initialize_firebase():
     if not firebase_admin._apps:  # Ensure Firebase is initialized only once
-        cred = credentials.Certificate("app\stressguard-firebase-adminsdk-5rxfz-0582c96f1b.json")
+        # Use a raw string or forward slashes in the file path
+        cred = credentials.Certificate(r"app/stressguard-firebase-adminsdk-5rxfz-0582c96f1b.json")
+        
         firebase_admin.initialize_app(cred, {
             'storageBucket': 'stressguard.appspot.com'
         })
