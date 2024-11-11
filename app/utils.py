4a00d930c845be718973b48e2534174f7c8c12b1
Lerato <lerato.rakgotho@gmail.com>
2024-11-11 21:35:15 +0200

    util

--- app/utils.py
+++ app/utils.py
@@ -1,22 +1,10 @@
 import firebase_admin
-from firebase_admin import credentials, firestore, storage
-import streamlit as st
-import json
+from firebase_admin import credentials
+import os
 
-# Function to initialize Firebase and return clients for Firestore and Storage
 def initialize_firebase():
     if not firebase_admin._apps:  # Ensure Firebase is initialized only once
-        # Load and parse credentials from Streamlit secrets
-        firebase_creds = json.loads(st.secrets["firebase_credentials"])
-        cred = credentials.Certificate(firebase_creds)
-        
-        # Initialize Firebase app with Firestore and Storage
+        cred = credentials.Certificate("app\stressguard-firebase-adminsdk-5rxfz-0582c96f1b.json")
         firebase_admin.initialize_app(cred, {
-            'storageBucket': firebase_creds["project_id"] + ".appspot.com"
+            'storageBucket': 'stressguard.appspot.com'
         })
-
-    # Initialize Firestore client and Storage bucket
-    db = firestore.client()
-    bucket = storage.bucket()
-    
-    return db, bucket
